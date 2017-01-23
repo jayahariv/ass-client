888d2cf5fb1e9b7fe6ede0b918ef01c9d44b5532
Jayahari Vavachan <jayahariv88@gmail.com>
2017-01-22 17:41:07 -0800

    Ass first page (#20)
    * readme modified
    
    * react app added
    
    * text edited
    
    * content chnaged
    
    * removing all files
    
    * once again

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
         <p className="App-intro">
           This is a sample test content section. In other terms, hello world paragraph. 
         </p>
