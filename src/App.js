af9ba861b3731b02e8f513a617c62740ace4cf42
Jayahari Vavachan <jayahariv88@gmail.com>
2017-01-24 09:42:40 -0800

    Ass first page (#27)
    * readme modified
    
    * react app added
    
    * text edited
    
    * content chnaged
    
    * removing all files
    
    * once again
    
    * read me changd
    
    * package json edited
    
    * text changed
    
    * class arrangement corrected

--- src/App.js
+++ src/App.js
@@ -1,72 +1,21 @@
+import DocumentTitle from 'react-document-title'
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import AssemblaAPI from './service/AssemblaAPI.js';
+import Login from './ui/LoginContainer.js';
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      key: '',
-      secret: '',
-    };
-
-    this.onChangeKey = this.onChangeKey.bind(this);
-    this.onChangeSecret = this.onChangeSecret.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  onChangeKey(event) {
-    this.setState({
-      key: event.target.value,
-    });
-  }
-
-  onChangeSecret(event) {
-    this.setState({
-      secret: event.target.value,
-    });
-  }
-
-  handleSubmit(event) {
-    alert('Key: ' + this.state.key + ' Secret: ' + this.state.secret);
-    AssemblaAPI.getActivity(
-      this.state.key,
-      this.state.secret,
-      function(err, resp) {
-        console.log(resp);
-      },
-    );
-    event.preventDefault();
-  }
-  
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Ass-Client</h2>
+      <DocumentTitle title="Ass-Client">
+        <div className="App" title="Home">
+          <div className="App-header">
+            <h2>Ass-Client</h2>
+          </div>
+          <p></p>
+          <Login />
         </div>
-        <p></p>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Key :
-            <input
-              type="text"
-              value={this.state.key}
-              onChange={this.onChangeKey} />
-          </label>
-          <label>
-            Secret :
-            <input
-              type="text"
-              value={this.state.secret}
-              onChange={this.onChangeSecret} />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
+      </DocumentTitle>
     );
   }
 }
