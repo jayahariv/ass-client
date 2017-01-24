d6d7bdb50b762779f2ffb44f99785c6ada5b4f3e
Jayahari Vavachan <jvavachan@fb.com>
2017-01-23 23:12:21 -0800

    assembla method added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AssemblaAPI from './service/AssemblaAPI.js';
+// const AssemblaAPI = require('./service/AssemblaAPI');
 
 class App extends Component {
   render() {
+    AssemblaAPI.getActivity(
+      function (err, data) {
+        if (err) {throw err}
+        console.log('result: ', data);
+      },
+    );
     return (
       <div className="App">
         <div className="App-header">
@@ -11,7 +19,7 @@ class App extends Component {
           <h2>Welcome to Ass-Client</h2>
         </div>
         <p className="App-intro">
-          This is a sample test content section. In other terms, hello world paragraph. 
+          This is a sample test content section. In other terms, hello world paragraph.
         </p>
       </div>
     );
