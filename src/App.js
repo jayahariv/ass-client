3e1c6a30006e476e7f1532b9ea21203224166614
Jayahari Vavachan <jvavachan@fb.com>
2017-01-24 17:36:33 -0800

    design changed

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import DocumentTitle from 'react-document-title'
 import React, { Component } from 'react';
 import './App.css';
 import Login from './ui/LoginContainer.js';
+import SideMenuContainer from './ui/SideMenuContainer.js';
 
 class App extends Component {
-
   render() {
     return (
       <DocumentTitle title="Ass-Client">
         <div className="App" title="Home">
           <div className="App-header">
-            <h2>Ass-Client</h2>
+            <div className="App-title">
+              Ass-Client
+            </div>
           </div>
-          <p></p>
-          <Login />
+          <div className="App-content">
+            <SideMenuContainer />
+            <Login />
+          </div>
         </div>
       </DocumentTitle>
     );
