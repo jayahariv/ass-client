10655791968704e2decd09f6d77d1c011eef5bca
Jayahari Vavachan <jvavachan@fb.com>
2017-02-07 21:13:56 -0800

    removed xxx

--- src/ui/LoginContainer.js
+++ src/ui/LoginContainer.js
@@ -19,8 +19,8 @@ class LoginContainer extends React.Component<void, Props, void> {
   constructor(props) {
     super(props);
     this.state = {
-      key: 'e68488890419de9a030d',
-      secret: '3875a0a5c1e73a97380c6dadbad884e9f0b1a3b4',
+      key: '',
+      secret: '',
     };
     this.onChangeKey = this.onChangeKey.bind(this);
     this.onChangeSecret = this.onChangeSecret.bind(this);
