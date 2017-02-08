805e43fcd317ca1056b763f0ad07389a28529d6f
Jayahari Vavachan <jayahariv88@gmail.com>
2017-02-07 22:00:15 -0800

    Origin (#52)
    * readme modified
    
    * readme modified (#15)
    
    * react app added
    
    * Ass first page (#16)
    
    * readme modified
    
    * react app added
    
    * text edited
    
    * Ass first page (#17)
    
    * readme modified
    
    * react app added
    
    * text edited
    
    * content chnaged
    
    * Ass first page (#18)
    
    * readme modified
    
    * react app added
    
    * text edited
    
    * content chnaged
    
    * removing all files
    
    * once again
    
    * Ass first page (#20)
    
    * readme modified
    
    * react app added
    
    * text edited
    
    * content chnaged
    
    * removing all files
    
    * once again
    
    * read me changd
    
    * Ass first page (#21)
    
    * readme modified
    
    * react app added
    
    * text edited
    
    * content chnaged
    
    * removing all files
    
    * once again
    
    * read me changd
    
    * package json edited
    
    * Ass first page (#22)
    
    * readme modified
    
    * react app added
    
    * text edited
    
    * content chnaged
    
    * removing all files
    
    * once again
    
    * read me changd
    
    * package json edited
    
    * text changed
    
    * Ass first page (#23)
    
    * readme modified
    
    * react app added
    
    * text edited
    
    * content chnaged
    
    * removing all files
    
    * once again
    
    * read me changd
    
    * package json edited
    
    * text changed
    
    * Ass service (#24)
    
    * readme modified
    
    * react app added
    
    * text edited
    
    * content chnaged
    
    * removing all files
    
    * once again
    
    * read me changd
    
    * package json edited
    
    * text changed
    
    * assembla method added
    
    * Ass service (#25)
    
    * readme modified
    
    * react app added
    
    * text edited
    
    * content chnaged
    
    * removing all files
    
    * once again
    
    * read me changd
    
    * package json edited
    
    * text changed
    
    * assembla method added
    
    * input fields added
    
    * Ass service (#26)
    
    * readme modified
    
    * react app added
    
    * text edited
    
    * content chnaged
    
    * removing all files
    
    * once again
    
    * read me changd
    
    * package json edited
    
    * text changed
    
    * assembla method added
    
    * input fields added
    
    * removed warning
    
    * class arrangement corrected
    
    * Ass first page (#27)
    
    * readme modified
    
    * react app added
    
    * text edited
    
    * content chnaged
    
    * removing all files
    
    * once again
    
    * read me changd
    
    * package json edited
    
    * text changed
    
    * class arrangement corrected
    
    * design changed
    
    * warnings removed
    
    * Ass first page (#28)
    
    * readme modified
    
    * react app added
    
    * text edited
    
    * content chnaged
    
    * removing all files
    
    * once again
    
    * read me changd
    
    * package json edited
    
    * text changed
    
    * class arrangement corrected
    
    * design changed
    
    * warnings removed
    
    * login section moved
    
    * Ass first page (#29)
    
    * readme modified
    
    * react app added
    
    * text edited
    
    * content chnaged
    
    * removing all files
    
    * once again
    
    * read me changd
    
    * package json edited
    
    * text changed
    
    * class arrangement corrected
    
    * design changed
    
    * warnings removed
    
    * login section moved
    
    * proxy handled
    
    * Ass first page (#30)
    
    * readme modified
    
    * react app added
    
    * text edited
    
    * content chnaged
    
    * removing all files
    
    * once again
    
    * read me changd
    
    * package json edited
    
    * text changed
    
    * class arrangement corrected
    
    * design changed
    
    * warnings removed
    
    * login section moved
    
    * proxy handled
    
    * callback is added as prop

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
+import DocumentTitle from 'react-document-title'
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Login from './ui/LoginContainer.js';
+import SideMenuContainer from './ui/SideMenuContainer.js';
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <DocumentTitle title="Ass-Client">
+        <div className="App" title="Home">
+          <div className="App-header">
+            <div className="App-title">
+              Ass-Client
+            </div>
+          </div>
+          <div className="App-content">
+            <SideMenuContainer />
+            <div className='App-middle' />
+            <Login
+              callback={($error, $resp) => {
+                
+              }}
+            />
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </DocumentTitle>
     );
   }
 }
---
+++ src/service/AssemblaAPI.js
@@ -0,0 +1,25 @@
+const request = require('xhr-request');
+
+class AssemblaAPI {
+  _baseUrl: string;
+  _corsProxy: string;
+  constructor() {
+    this._corsProxy = 'http://localhost:1337';
+    this._baseUrl = '/api.assembla.com';
+  }
+
+  getActivity(key: string, secret: string, callback: Function): string {
+    request(
+      this._corsProxy + this._baseUrl + '/v1/activity/',
+      {
+        headers: {
+          'X-Api-Key': key,
+          'X-Api-Secret': secret,
+        },
+      },
+      callback,
+    );
+  }
+}
+
+module.exports = new AssemblaAPI();
