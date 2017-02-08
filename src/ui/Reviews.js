6f6f4bb03fc0ca73c268eb7b6f384ba2176df68b
Jayahari Vavachan <jvavachan@fb.com>
2017-02-07 21:13:27 -0800

    reviews are added

--- src/ui/Reviews.js
+++ src/ui/Reviews.js
@@ -1,26 +1,103 @@
+import AssemblaAPI from '.././service/AssemblaAPI.js';
 import React from 'react'
 
-class Reviews extends React.Component {
-  static PropTypes = {
-    callback: PropTypes.func.isRequired,
-  };
+const {PropTypes} = React;
+
+type Props = {}
+
+const style = {
+  table: {
+    'display': 'block',
+    'height': '660px',
+    'overflowY': 'scroll',
+    'width' : '100%',
+  },
+  hRow: {
+    'backgroundColor': '#4CAF50',
+    'color': 'white',
+    'width': '10%',
+  },
+  hRowTitle: {
+    'backgroundColor': '#4CAF50',
+    'color': 'white',
+    'width': '60%',
+  },
+  row: {
+    'text-align': 'left',
+  }
+}
+
+class Reviews extends React.Component<Props> {
+  props: Props;
+
+  static PropTypes = {};
 
   constructor(props) {
     super(props);
     this.state = {
       reviews: [],
     };
+    this._activityFetched = this._activityFetched.bind(this);
+    this._trim = this._trim.bind(this);
   }
 
   componentDidMount() {
-    // call service to load all the reviews done by me!!
+    AssemblaAPI.getActivity(
+      this._activityFetched,
+    );
+  }
+
+  _activityFetched($e, $r) {
+    const a = JSON.parse($r);
+    console.log(a);
+    this.setState({
+      reviews: a.filter((obj) => {
+        return (
+          obj.operation === 'commented on' &&
+          obj.author_id === 'anAw7cx2Wr4jZneJe5cbLr'
+        );
+      }),
+    });
+  }
+
+  _content() {
+    const div = this.state.reviews.map((element, index, array) => {
+      return (
+        <tr key={index}>
+          <td style={style.row}>
+            <a href={element.url}>#{element.object_id}</a>
+          </td>
+          <td style={style.row}>{new Date(element.date).toLocaleString()}</td>
+          <td style={style.row}>{element.author_name}</td>
+          <td style={style.row}>{element.comment_or_description}</td>
+        </tr>
+      );
+    });
+    return (
+      <table style={style.table}>
+        <tbody>
+          <tr>
+            <th style={style.hRow}> ID </th>
+            <th style={style.hRow}> Date </th>
+            <th style={style.hRow}> Author </th>
+            <th style={style.hRowTitle}> Comment / Description </th>
+          </tr>
+          {div}
+        </tbody>
+      </table>
+    );
   }
 
   render() {
-    return (
-      // show all the reviews by me!!
-      <div> Rewviews</div>
-    );
+    return this._content();
+  }
+
+  _trim(s, padding, l) {
+    return s.length > l ? s.substring(padding, l - padding) + '...' : s;
+  };
+
+  _date(d): string {
+    return
   }
 }
 
